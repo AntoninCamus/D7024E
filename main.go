dc50668a8ea0b9489acea82f4bd291f7e8b52ee4
Antonin Camus <antonin.cms@gmail.com>
2019-09-20 10:46:27 +0200

    Added signal waiting (to handle future async servers)

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/LHJ/D7024E/kademlia/networking"
+import (
+	"google.golang.org/grpc"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/LHJ/D7024E/kademlia/networking"
+)
+
+type ManagementSingleton struct {
+	sigChan chan os.Signal
+	restSrv http.Server
+	grpcSrv grpc.Server
+}
 
 func main() {
+	mgmtSing := ManagementSingleton{
+		sigChan: make(chan os.Signal, 1),
+	}
+
+	// TODO Servers calls should be taking as argument the signal chan and return server (or a specific structure)
 	networking.StartRestServer()
+	//networking.StartGrpcServer()
+
+	// Setup signal handling
+	signal.Notify(mgmtSing.sigChan, os.Interrupt)
+	<-mgmtSing.sigChan // Wait for exit signal
+	// Clean
 }
