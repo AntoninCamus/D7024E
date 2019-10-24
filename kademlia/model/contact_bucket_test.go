2247597260c0a22f2437ee05b1866c400d999eec
Jens Lindholm <lh@ludd.ltu.se>
2019-10-24 22:56:53 +0200

    Added contact_bucket testing and partial contact testing

---
+++ kademlia/model/contact_bucket_test.go
@@ -0,0 +1,96 @@
+package model
+
+import (
+	"testing"
+)
+
+func TestNewBucket(t *testing.T){
+	bucket1 := newBucket()
+	bucket2 := newBucket()
+	if bucket1 == bucket2 {
+		t.Errorf("The two new buckets are the same object")
+	}
+}
+func TestAddContact(t *testing.T){
+	testBucket := newBucket()
+
+	testContact1 := newContact(NewRandomKademliaID(), "someaddress")
+	testBucket.addContact(testContact1)
+
+	testContact2 := newContact(NewRandomKademliaID(), "someotheraddress")
+	testBucket.addContact(testContact2)
+
+
+	if testBucket.list.Front().Value.(Contact).ID != testContact1.ID && testBucket.list.Back().Value.(Contact).ID != testContact1.ID {
+		//log.Print(testBucket.list.Front().Value.(Contact).ID) first not found anywhere
+		//log.Print(testBucket.list.Back().Value.(Contact).ID)
+		t.Errorf("addContact didn't add the contacts with id %s to the list, or it has some other values", testContact1.ID.String())
+	}else if testBucket.list.Front().Value.(Contact).ID != testContact2.ID && testBucket.list.Back().Value.(Contact) != testContact2{
+		t.Errorf("addContact didn't add the contact with id %s to the list, or it has some other values too", testContact2.ID.String())
+	}
+
+	testBucket.addContact(testContact1)
+	if testBucket.list.Front().Value != testContact1 {
+		t.Errorf("testContact1 was not moved to the front")
+	}
+	//for i := 0; testBucket.len() < BucketSize; i++{
+	//	testBucket.addContact(newContact(NewRandomKademliaID(), "address " + string(i)))
+	//}
+
+
+
+}
+
+func TestRemoveContact(t *testing.T){
+	testBucket := newBucket()
+	testContact := newContact(NewRandomKademliaID(), "someaddress")
+	initialLen := testBucket.len()
+	testBucket.addContact(testContact)
+	addedLen := testBucket.len()
+	if addedLen <= initialLen{
+		t.Errorf("Bucket did not increase in size")
+	}
+	testBucket.removeContact(testContact)
+	if addedLen < testBucket.len(){
+		t.Errorf("Bucket did not decrease in size")
+	}
+}
+func TestGetContactAndCalcDistance(t *testing.T){
+	testBucket := newBucket()
+	testContact := newContact(NewRandomKademliaID(), "someaddress")
+	testBucket.addContact(testContact)
+
+	targetContact := newContact(NewRandomKademliaID(), "targetAddress")
+
+	rangedContacts := testBucket.getContactAndCalcDistance(targetContact.ID)
+
+	//really, is it the job of this test to check that the CalcDistance does it's job?
+
+	if len(rangedContacts) != testBucket.len(){
+		t.Errorf("getContactAndCalcDistance returned an array of contacts of different size than the buckets")
+	}
+
+}
+
+func TestLen(t *testing.T){
+	testBucket := newBucket()
+	testContact := newContact(NewRandomKademliaID(), "someaddress")
+	initialLen := testBucket.len()
+	testBucket.addContact(testContact)
+	addedLen := testBucket.len()
+	if addedLen <= initialLen{
+		t.Errorf("Bucket did not increase in size")
+	}
+}
+func TestString(t *testing.T){
+	testBucket := newBucket()
+	interfaceString := testBucket.string()
+	if interfaceString != "[]"{
+		t.Errorf("string() didn't return an empty []")
+	}
+	testBucket.addContact(newContact(NewRandomKademliaID(), "teststring"))
+	if testBucket.string() == "[]"{
+		t.Errorf("string() didn't print out new contact")
+	}
+
+}
---
+++ kademlia/model/contact_test.go
@@ -0,0 +1,65 @@
+package model
+
+import "testing"
+
+func TestNewContact(t *testing.T)  {
+	testContact1 := newContact(NewRandomKademliaID(), "someaddress")
+	testContact2 := newContact(NewRandomKademliaID(), "someaddress")
+
+	if testContact1 == testContact2 {
+		t.Errorf("The two new contacts are the same object")
+	}
+}
+func TestContact_CalcDistance(t *testing.T) {
+	testContact1 := newContact(NewRandomKademliaID(), "someaddress")
+	testContact2 := newContact(NewRandomKademliaID(), "someaddress")
+	//testContact3 := newContact(NewRandomKademliaID(), "someaddress")
+
+	distance := testContact1.ID.calcDistance(testContact2.ID)
+	testContact1.CalcDistance(testContact2.ID)
+	distance2 := testContact1.distance
+
+	//distance3 := testContact1.ID.calcDistance(testContact3.ID)
+
+	if *distance != *distance2 {
+		t.Errorf("Calculated distance %s is not assigned to the contact but instead %s", distance, distance2)
+	}
+}
+
+func TestContactCandidates_Len_Append(t *testing.T) {
+	testContact1 := newContact(NewRandomKademliaID(), "someaddress")
+	contactArray1 := []Contact{testContact1}
+
+	testContact2 := newContact(NewRandomKademliaID(), "someaddress")
+	contactArray2 := []Contact{testContact2}
+
+	contacts := contactCandidates{contactArray1}
+
+	shortDistance := contacts.Len()
+	contacts.append(contactArray2)
+	longDistance := contacts.Len()
+
+	if shortDistance >= longDistance {
+		t.Errorf("Append did not append the contact, or Len did not return the correct length")
+	}
+}
+
+func TestGetContacts(t *testing.T){
+	testContact1 := newContact(NewRandomKademliaID(), "someaddress")
+	testContact2 := newContact(NewRandomKademliaID(), "someaddress")
+	testContact3 := newContact(NewRandomKademliaID(), "someaddress")
+	contactArray1 := []Contact{testContact1}
+	contactArray2 := []Contact{testContact2, testContact3}
+	verificationArray := []Contact{testContact1, testContact2}
+
+	contactCanditateArray := contactCandidates{contactArray1}
+	contactCanditateArray.append(contactArray2)
+
+	returnContacts := contactCanditateArray.getContacts(2)
+	for index, contact := range returnContacts{
+		if contact != verificationArray[index]{
+			t.Errorf("getContacts did not return the same array as input")
+		}
+	}
+}
+
