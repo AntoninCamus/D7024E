7568cd82ca9b89d21cd85eeddf70fcf4891b4e56
Antonin Camus <antonin.cms@gmail.com>
2019-10-27 22:25:21 +0100

    Updated client

--- tools/client.py
+++ tools/client.py
@@ -4,17 +4,23 @@ import sys
 import requests
 from optparse import OptionParser
 
-def store(url : str, file : str):
-    print("Storing {} onto {}...".format(file, url), file=sys.stderr)
-    response = requests.post(url+"/kademlia/file", file)
-    print("Status={}".format(response.status_code) , file=sys.stderr)
-    return response.json()
 
-def find(url : str, id : str):
+def store(url: str, file: str):
+    print("Storing {} onto {}...".format(file, url), file=sys.stderr)
+    response = requests.post(url + "/kademlia/file", file)
+    print("Status={}".format(response.status_code), file=sys.stderr)
+    return response.json() if response.status_code == 200 else "Could not store {}".format(file)
+
+
+def find(url: str, id: str):
     print("Retrieving {} from {}...".format(id, url), file=sys.stderr)
-    response = requests.get(url+"/kademlia/file", params={ 'id' : id })
-    print("Status={}".format(response.status_code) , file=sys.stderr)
-    return response.json()
+    try:
+        response = requests.get(url + "/kademlia/file", params={'id': id})
+        print("Status={}".format(response.status_code), file=sys.stderr)
+        return response.json() if response.status_code == 200 else "File {} not found".format(id)
+    except:
+        return "{} is an invalid ID".format(id)
+
 
 if __name__ == "__main__":
     usage = "usage: %prog [options] command IP:port"
@@ -33,21 +39,21 @@ if __name__ == "__main__":
         parser.error("No address specified")
     url = args[1]
     if url[0:7] != "http://":
-        url = "http://"+url
+        url = "http://" + url
 
     # Parse command
     if cmd == "store" or cmd == "find":
         if (cmd == "store" and not options.file):
             parser.error("No file specified to store")
-        elif (cmd=="find" and not options.id):
+        elif (cmd == "find" and not options.id):
             parser.error("No id specified to find")
         else:
             print(store(url, options.file) if cmd == "store" else find(url, options.id))
 
     elif cmd == "exit":
         print("Calling...")
-        response = requests.post(url+"/node/exit")
+        response = requests.post(url + "/node/exit")
         print("Status: ", response.status_code)
 
     else:
-        print("Command ", cmd ," not found")
+        print("Command ", cmd, " not found")
